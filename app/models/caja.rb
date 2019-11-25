ce2a34743a7d65bf898dd8c9d3f9de94ae07f89a
Mario Villarroel <movilla@gmail.com>
2019-11-25 14:11:56 -0300

    Fixed validation, latest erd

--- app/models/caja.rb
+++ app/models/caja.rb
@@ -2,7 +2,7 @@ class Caja < ApplicationRecord
   belongs_to :creador, class_name: "Usuario"
   validates :fondo_inicial, numericality: { greater_than: 0 }
   validates :fecha, presence: true
-  validates :fecha_actual_o_futuro
+  validate :fecha_actual_o_futuro
 
   private
 
